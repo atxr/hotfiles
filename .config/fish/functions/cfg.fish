df15b4820e160296a53470a038335768bb16fd86
Alexandre Tullot <at@locaterisk.com>
2022-12-12 16:01:52 +0100

    Add mkalias and cfg aliases to fish

---
+++ .config/fish/functions/cfg.fish
@@ -0,0 +1,3 @@
+function cfg --wraps='/usr/bin/git --git-dir=/home/atxr/.hotfiles --work-tree=/home/atxr' --description 'alias cfg=/usr/bin/git --git-dir=/home/atxr/.hotfiles --work-tree=/home/atxr'
+  /usr/bin/git --git-dir=/home/atxr/.hotfiles --work-tree=/home/atxr $argv; 
+end
