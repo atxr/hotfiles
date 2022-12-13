e2f680f35b81f443e4851d4c8657d7893e964e41
atxr <alexandre.tullot@protonmail.com>
2022-12-13 01:20:44 +0100

    Add first config of nvim
    Setup Telescope

---
+++ .config/nvim/lua/atxr/init.lua
@@ -0,0 +1,9 @@
+require('atxr.packer')
+require('atxr.telescope')
+require('atxr.options')
+require('atxr.colors')
+require('atxr.highlights')
+require('atxr.maps')
+require('atxr.lsp')
+
+vim.opt.clipboard:append { 'unnamedplus' }
---
+++ .config/nvim/lua/atxr/maps.lua
@@ -0,0 +1,30 @@
+local keymap = vim.keymap
+
+-- Disable yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Increment/Decrement
+keymap.set('n', '+', '<C-a>')
+keymap.set('n', '-', '<C-x>')
+
+-- Select all
+keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- New tab
+keymap.set('n', 'te', ':tabedit<Return>')
+
+-- Split window
+keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+keymap.set('', 'sh', '<C-w>h')
+keymap.set('', 'sk', '<C-w>k')
+keymap.set('', 'sj', '<C-w>j')
+keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+keymap.set('n', 's<left>', '<C-w><')
+keymap.set('n', 's<right>', '<C-w>>')
+keymap.set('n', 's<up>', '<C-w>+')
+keymap.set('n', 's<down>', '<C-w>-')
