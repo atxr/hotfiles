c1f484b464e20e961e47995ac055f270aadd03a4
atxr <alexandre.tullot@protonmail.com>
2022-12-15 00:17:13 +0100

    Add mappings to nvim
    Add quick reload
    Add close window

--- .config/nvim/lua/atxr/maps.lua
+++ .config/nvim/lua/atxr/maps.lua
@@ -1,7 +1,10 @@
 local keymap = vim.keymap
 
+-- Reload config
+keymap.set('n', '<F4>', ':lua package.loaded.atxr = nil\n:source ~/.config/nvim/init.vim\n')
+
 -- Disable yank with x
-keymap.set('n', 'x', '"_x')
+keymap.set('n', 'x', '_x')
 
 -- Increment/Decrement
 keymap.set('n', '+', '<C-a>')
@@ -12,6 +15,9 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
 keymap.set('n', 'te', ':tabedit<Return>')
+
+-- Close window
+keymap.set('n', 'sc', '<C-w>q')
 
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
